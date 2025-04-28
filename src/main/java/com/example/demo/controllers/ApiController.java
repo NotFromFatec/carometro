c8a1945159c7c9b3dfba480bbcc79b8da6c90832
NotFromFatec <162596178+NotFromFatec@users.noreply.github.com>
2025-04-28 20:10:12 -0300

    Fix api/v1// issue, better api example, new dep

--- src/main/java/com/example/demo/controllers/ApiController.java
+++ src/main/java/com/example/demo/controllers/ApiController.java
@@ -2,28 +2,32 @@ package com.example.demo.controllers;
 
 import java.util.Map;
 
-import org.json.JSONObject;
-
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.google.gson.Gson;
+
 import org.springframework.web.bind.annotation.RequestHeader;
+
+class Fabio {
+    int inteligencia = 2;
+    boolean bolsa = true;
+    double altura = 3.0;
+}
 
 @RestController
 public class ApiController {
-    @GetMapping("/api/v1/")
+    @GetMapping("/api/v1/egressos")
 	public ResponseEntity<String> index(@RequestHeader Map<String, String> headers) {
         HttpHeaders responseHeaders = new HttpHeaders();
         responseHeaders.set("Content-Type", "application/json");
 
-        JSONObject jo = new JSONObject();
+        Gson gson = new Gson();  
+        String userJson = gson.toJson(new Fabio());  
 
-        jo.put("name", "jon doe");
-        jo.put("age", "22");
-        jo.put("city", "chicago");
-
-        return new ResponseEntity<String>(jo.toString(), responseHeaders, HttpStatus.CREATED);
+        return new ResponseEntity<String>(userJson, responseHeaders, HttpStatus.CREATED);
 	}
 }
